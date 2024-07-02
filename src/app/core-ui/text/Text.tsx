299d90f1ebaae3a7516e1c097f17270e42e14514
Jopras21 <sun.phin098@gmail.com>
2024-07-02 12:32:55 +0700

    core-ui/text update

---
+++ src/app/core-ui/text/Text.tsx
@@ -0,0 +1,26 @@
+import './Text.css';
+
+type Props = {
+  size?: 'header' | 'normal' | 'small';
+  title: string;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export function Text(props: Props) {
+  let {size = 'normal', title, className, style} = props;
+
+  const baseClass =
+    size === 'header'
+      ? 'header-text'
+      : size === 'normal'
+      ? 'normal-text'
+      : 'small-text';
+  const combinedClass = className ? `${baseClass} ${className}` : baseClass;
+
+  return (
+    <span className={combinedClass} style={style}>
+      {title}
+    </span>
+  );
+}
