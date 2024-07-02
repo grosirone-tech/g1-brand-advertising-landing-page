dac88ac33ed71e6792fceb19a1e40160e6845e30
Jopras21 <sun.phin098@gmail.com>
2024-07-02 15:37:28 +0700

    rapihin footer (footer bagian bawah)

--- src/app/footer/Footer.tsx
+++ src/app/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import './footer.css';
 import {Text} from '../core-ui/text/Text';
-import SocialButton from '../core-ui/button/SocialButton.tsx';
 
 export default function Footer() {
   return (
@@ -20,7 +19,37 @@ export default function Footer() {
             title="Connect with us"
             style={{textAlign: 'center', marginBottom: '.5rem'}}
           />
-          <SocialButton />
+          <div className="social-icon">
+            <ul className="social-wrapper">
+              <a href="#">
+                <li className="icon linkedin">
+                  <span className="tooltip">Linkedin</span>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/61/61109.png"
+                    className="linkedin"
+                  ></img>
+                </li>
+              </a>
+              <a href="#">
+                <li className="icon twitter">
+                  <span className="tooltip">Twitter</span>
+                  <img
+                    src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/twitter-icon.png"
+                    className="twitter"
+                  ></img>
+                </li>
+              </a>
+              <a href="#">
+                <li className="icon instagram">
+                  <span className="tooltip">Instagram</span>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png"
+                    className="instagram"
+                  ></img>
+                </li>
+              </a>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
