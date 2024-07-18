f5f05cba7ad272a1649260784b6602b2d192c22e
Jopras21 <sun.phin098@gmail.com>
2024-07-18 18:19:10 +0700

    core-ui button making primary button with the specified background color with a minimalis hover to make it simple

---
+++ src/core-ui/button/Button.tsx
@@ -0,0 +1,25 @@
+import './button.css';
+import {Text} from '../text/Text';
+
+type Props = {
+  type?: 'primary' | 'secondary';
+  onClick?: () => void;
+  content: string;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export default function Button(props: Props) {
+  const {type, content, className, style, onClick} = props;
+
+  return (
+    <button
+      type="button"
+      className={type === 'primary' ? 'primary-button' : 'secondary-button'}
+      style={style}
+    >
+      <div className="button-animation email-icon"></div>
+      <Text size="normal" content={content} />
+    </button>
+  );
+}
