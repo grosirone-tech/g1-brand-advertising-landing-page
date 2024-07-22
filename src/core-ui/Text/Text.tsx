07e43282016d53b0d077b758f3a8126b81d08c14
Abigail Vania <abigailvaniapra@gmail.com>
2024-07-22 17:18:03 +0700

    core-ui text add a new type, navbar

--- src/core-ui/Text/Text.tsx
+++ src/core-ui/Text/Text.tsx
@@ -1,18 +1,20 @@
 import './text.css';
 
 type Props = {
-  size: 'header' | 'subheader' | 'normal' | 'small';
+  size: 'header' | 'subheader' | 'normal' | 'navbar' | 'small' ;
   content: string;
   className?: string;
   onClick?: () => void;
+  style?: React.CSSProperties;
 };
 
 export function Text(props: Props) {
-  let {size, content, className, onClick} = props;
+  let {size, content, className, onClick, style} = props;
 
   const baseClass = size === 'header' ? 'header-text'
                 : size === 'subheader' ? 'subheader-text'
-                : size === 'normal' ? 'normal-text' : 'small-text';
+                : size === 'normal' ? 'normal-text'
+                : size === 'navbar' ? 'navbar-text' : 'small-text';
     const combinedClass = className ? `${baseClass} ${className}` : baseClass;
 
   return (
