10b851a527cbb2cad9dd506473a466e8121d7160
Jopras21 <sun.phin098@gmail.com>
2024-07-30 14:37:48 +0700

    scenes our technology changing the assets to a tablet and solid background color

--- src/scenes/ourTechnology/OurTechnology.tsx
+++ src/scenes/ourTechnology/OurTechnology.tsx
@@ -4,35 +4,9 @@ import React, {useEffect, useRef} from 'react';
 import './ourTechnology.css';
 import {Text} from '../../core-ui/text/Text';
 
-const OurTechnology: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const imageRef = useRef<HTMLDivElement>(null);
-
-  const handleScroll = () => {
-    if (imageRef.current) {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      const parallaxOffset = scrollTop * 0.1;
-
-      imageRef.current.style.transform = `translateY(${parallaxOffset}px)`;
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+export default function Technology() {
   return (
-    <section className="our-technology" id="tech" ref={containerRef}>
-      <div className="parallax-background" ref={imageRef}>
-        <img src="https://img.lovepik.com/background/20211021/large/lovepik-simple-technology-background-image_401491786.jpg" />
-      </div>
-      {/* Content */}
+    <section className="our-technology" id="tech">
       <Text
         size="subheader"
         content="TEKNOLOGI KAMI"
@@ -41,7 +15,7 @@ const OurTechnology: React.FC = () => {
       <div className="our-technology-container">
         <div className="our-technology-img">
           <img
-            src="https://www.twintel.net/wp-content/uploads/2023/10/10-25-23.jpg"
+            src="../tablet.png"
             alt="Technology"
           />
         </div>
@@ -59,4 +33,3 @@ const OurTechnology: React.FC = () => {
   );
 };
 
-export default OurTechnology;
