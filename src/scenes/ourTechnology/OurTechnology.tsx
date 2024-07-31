bc8ff6bfc59db9b79f1c3f1f3ee61ea711012445
Jopras21 <sun.phin098@gmail.com>
2024-07-31 12:34:35 +0700

    scenes our technology adding tablet assets and adjusting the styling

--- src/scenes/ourTechnology/OurTechnology.tsx
+++ src/scenes/ourTechnology/OurTechnology.tsx
@@ -1,10 +1,22 @@
 'use client';
 
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import './ourTechnology.css';
 import {Text} from '../../core-ui/text/Text';
 
 export default function Technology() {
+  const techItems = [
+    {id: '1', spec: 'Ukuran Layar', desc: '15 inch'},
+    {id: '2', spec: 'Ukuran Pixel', desc: '1080 x 1920 pixels'},
+    {id: '3', spec: 'Jenis Iklan', desc: 'Short Video'},
+    {id: '4', spec: 'Jam Operasional Spot', desc: '6AM-3PM'},
+    {id: '5', spec: 'Market Exposure', desc: '108 Spot/titik'},
+    {id: '6', spec: 'Durasi per Iklan Orientasi', desc: '2 Markets Exposure'},
+    {id: '7', spec: 'Rotasi Iklan', desc: '30 detik (max)'},
+    {id: '8', spec: 'Rotasi Iklan', desc: 'Landscape'},
+    {id: '9', spec: 'Rotasi Iklan', desc: '5 menit (10 iklan/titik)'},
+  ];
+
   return (
     <section className="our-technology" id="tech">
       <Text
@@ -14,22 +26,39 @@ export default function Technology() {
       />
       <div className="our-technology-container">
         <div className="our-technology-img">
-          <img
-            src="../tablet.png"
-            alt="Technology"
-          />
+          <img src="../tablet.png" alt="Technology" />
         </div>
         <div className="our-technology-text">
-          <Text size="normal" content="Spesifikasi" style={{height: '3rem'}} />
-          <Text size="small" content="Ukuran Layar" />
-          <Text size="small" content="Jenis Iklan" />
-          <Text size="small" content="Jam Oprasional Spot" />
-          <Text size="small" content="Market Exposure" />
-          <Text size="small" content="Durasi per Iklan Orientasi" />
-          <Text size="small" content="Rotasi Iklan" />
+          <table className="technology-text">
+            <thead>
+              <tr style={{textAlign: 'center', width: '100%', height: '3rem'}}>
+                <th colSpan={3}>
+                  <Text
+                    size="normal"
+                    content="Spesifikasi"
+                    className="spec-title"
+                  />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {techItems.map(({id, spec, desc}) => (
+                <tr key={id}>
+                  <td>
+                    <Text size="small" content={spec} />
+                  </td>
+                  <td>
+                    <Text size="normal" content=" : " />
+                  </td>
+                  <td>
+                    <Text size="small" content={desc} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
   );
-};
-
+}
