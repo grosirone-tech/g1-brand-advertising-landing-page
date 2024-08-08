9f5eb221ae68614a3bd3410d4574e111d1e734be
Jopras21 <sun.phin098@gmail.com>
2024-08-08 11:10:40 +0700

    core-ui text content

--- src/core-ui/text/Text.tsx
+++ src/core-ui/text/Text.tsx
@@ -1,8 +1,9 @@
+import {ReactNode} from 'react';
 import './text.css';
 
 type Props = {
-  size: 'header' | 'subheader' | 'normal' | 'navbar' | 'small' ;
-  content: string;
+  size: 'header' | 'subheader' | 'normal' | 'navbar' | 'small';
+  content: string | ReactNode;
   className?: string;
   onClick?: () => void;
   style?: React.CSSProperties;
@@ -11,16 +12,21 @@ type Props = {
 export function Text(props: Props) {
   let {size, content, className, onClick, style} = props;
 
-  const baseClass = size === 'header' ? 'header-text'
-                : size === 'subheader' ? 'subheader-text'
-                : size === 'normal' ? 'normal-text'
-                : size === 'navbar' ? 'navbar-text' : 'small-text';
-    const combinedClass = className ? `${baseClass} ${className}` : baseClass;
+  const baseClass =
+    size === 'header'
+      ? 'header-text'
+      : size === 'subheader'
+      ? 'subheader-text'
+      : size === 'normal'
+      ? 'normal-text'
+      : size === 'navbar'
+      ? 'navbar-text'
+      : 'small-text';
+  const combinedClass = className ? `${baseClass} ${className}` : baseClass;
 
   return (
     <span className={combinedClass} onClick={onClick} style={style}>
-        {content}
+      {content}
     </span>
   );
 }
-;
